f4261d0d4c99b853142d3e60a504ae7df90992ef
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-06-15 15:22:37 +1200

    css and html tidy up

--- seeds/choices.js
+++ seeds/choices.js
@@ -5,9 +5,18 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('choices').insert([
-        {scene_id: 1, choice_id: 1, text: 'This is your first choice', points: 8, consequences: 'Some awesome shit happens to you'},
-        {scene_id: 1, choice_id: 2, text: 'This is your second choice', points: 5, consequences: 'Some meh shit happens to you'},
-        {scene_id: 1, choice_id: 3, text: 'This is your third choice', points: 2, consequences: 'Some terrible shit happens to you'}
+        {scene_id: 1, choice_id: 1, text: 'Now. Plenty of time to get ready and take a few minutes to prepare for the day.', points: 8, consequences: 'Look, I don’t know how manage to go to bed early enough to pull this off, but I hate you. Lose 2 points.'},
+        {scene_id: 1, choice_id: 2, text: 'In 30 minutes. You’ll have just enough time to shower and get there on time-ish, and the extra sleep will help', points: 5, consequences: 'You arrive at 8:40, which is not late for the lecture at least. No points.'},
+        {scene_id: 1, choice_id: 3, text: 'In one hour. Showers are optional.', points: 2, consequences: 'That’s gross, you’re gross. Lose 10 points.'},
+
+        {scene_id: 2, choice_id: 4, text: 'My greatest weakness is that I’m a perfectionist. I try too hard.', points: 2, consequences: 'Boo. Everyone hates your lame cliches. Lose 3 points.'},
+        {scene_id: 2, choice_id: 5, text: 'I think my greatest weakness is that the Obama Administration and the liberal media are using fake news to undermine me. A lot of people have told me that if it wasn’t for that, everything would be really great, really amazing. You’ll see', points: 2, consequences: 'Hello Mr President, wow, it’s really amazing that you’re here. I would have thought you had more important things to do, like arranging a summit with North Korea or tweeting about how much you hate Rosie O’Donnell. Lose 10 points.'},
+        {scene_id: 2, choice_id: 6, text: 'A big weakness for me is that I’m actually a cosmic horror from beyond mortal constraints of space and time, temporarily incarcerated in humanlike form, here to usher in a new age of cephalopodic rule over this insignificant sphere. That might conflict with the learning goals of EDA.', points: 2, consequences: 'Everyone laughs - they think you’re joking! Your cover is seemingly still safe. I guess honesty really is the best policy? Gain 3 points.'},
+
+        {scene_id: 2, choice_id: 7, text: 'Deflect with Humor: “Um, that variable should be renamed to Harrison?', points: 2, consequences: 'Everyone laughs, but Harrison’s eyes narrow. He’s on to your tricks, but what will the consequences be? 0 points'},
+        {scene_id: 2, choice_id: 8, text: 'That’s a great question, and I’m glad you asked. Let’s use a collaborative problem solving paradigm to achieve our learning objectives - what does the room think?', points: 2, consequences: 'Everyone sees through your obvious attempt to cover your tracks. Lose 5 points.'},
+        {scene_id: 2, choice_id: 9, text: 'I’m sorry, I’m not confident on this material, can you go back over it before I answer the question?', points: 2, consequences: 'A bunch of other people nod their heads and you can tell that they appreciate the chance to go over the material. Earn 5 points.'},
+
       ]);
     });
 };
--- seeds/scenes.js
+++ seeds/scenes.js
@@ -5,9 +5,13 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('scenes').insert([
-        {id: 1, title: 'title 1', text: 'This is scene 1', next: 2, image: 'https://images-na.ssl-images-amazon.com/images/I/91pzkLOWEoL._SX355_.jpg'},
-        {id: 2, title: 'title 2', text: 'This is scene 2', next: 3, image: 'https://images-na.ssl-images-amazon.com/images/I/81YZDy5lXzL._SL1500_.jpg'},
-        {id: 3, title: 'title 3', text: 'This is scene 3', next: 1, image: 'https://www.newdinosaurs.com/wp-content/uploads/2016/01/28_stegosaurus_karen_carr.jpg'}
+        {id: 1, title: 'Morning Before Class', text: 'Your alarm goes off. It’s one hour before you need to leave for EDA. When are you going to get up?', next: 2, image: 'https://media.istockphoto.com/vectors/little-girl-waking-up-vector-id802098412?k=6&m=802098412&s=612x612&w=0&h=lDF_JwGHl2NS3LGHKK3pg1mCl4abKmKfw_M-UjwBKFY='},
+        {id: 2, title: 'Accountability', text: 'As you arrive at class, you see the question on the board is ‘what is your greatest weakness?’ How do you answer the group?', next: 2, image: ''},
+        {id: 3, title: 'Morn Lecture/Floor Exercise', text: 'This morning’s lecture is with Harrison. You’re not all that confident of the material, and despite your best efforts, you zone for a few minutes when he explains something really complex, like for loops or something. Just as (you are deep in some particularly deep thoughts about your hero, Beyonce)/(a particularly exciting daydream involving Channing Tatum), Harrison turns to you and says “How do you think we should solve this problem?” What do you say?', next: 2, image: 'http://www.cnatrainingclass.com/wp-content/uploads/2012/07/confused-sleeping-at-computer1.jpg'},
+
+        {id: 4, title: 'Yoga', text: 'Ming rings the bell of enlightenment. You look at your paring partner and gasp. Is it that time already? You quickly save add commit and push so you do not lose your work over lunch. Are you participating today?', next: 2, image: 'http://www.lexiyoga.com/images/partner-yoga.jpg'},
+        {id: 5, title: 'Lunch', text: 'Eating time and free reign is upon us. With the world at your feet you develop a plan. It is sunny outside and the birds are chirping. What do you do for lunch?', next: 3, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvZ765B5i5VTPO7034VypbhfeUmXngSzbH2TNvlYbuw57Qp1QL'},
+        {id: 6, title: 'Afternoon Lecture', text: 'We enter the room and our fearless leader sits up the front at the whiteboard. He smiles as you choose a place to worship him in the room. You sit and smile back. He gathers everyone’s minds with a deafening silence. Then he starts to speak. Do you:', next: 1, image: 'https://fingertipstyping.co.uk/wp-content/uploads/2017/04/PowerpointLecture-700x313.jpg'}
       ]);
     });
 };
